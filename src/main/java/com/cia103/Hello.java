0050de54be9e8aa7e98af07da4e6ae7363538c4a
P.J.Chen <lulu.chen231@gmail.com>
2024-10-23 16:20:43 +0800

    restart

--- src/main/java/com/cia103/Hello.java
+++
@@ -1,8 +0,0 @@
-package com.cia103;
-
-public class Hello {
-	public static void main(String[] args) {
-		System.out.println("G5 getting start....");
-	}
-	
-}
--- src/main/java/com/cia103/util/Point.java
+++
@@ -1,6 +0,0 @@
-package com.cia103.util;
-
-public class Point {
-
-    public static void givePoint(String userId, int point) {}
-}
