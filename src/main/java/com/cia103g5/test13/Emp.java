82334c786041ccd22cfd7c57cf36248271ea2287
Ivy Lo <lulu30416@gmail.com>
2024-10-24 23:44:29 +0800

    test2

---
+++ src/main/java/com/cia103g5/test13/Emp.java
@@ -0,0 +1,21 @@
+package com.cia103g5.test13;
+
+public class Emp {
+    private Integer empno;
+    private String ename;
+
+    public void setEmpno(Integer empno){
+        this.empno=empno;
+    }
+    public Integer getEmpno(){
+        return empno;
+    }
+
+    public void setEname(String ename) {
+        this.ename = ename;
+    }
+
+    public String getEname() {
+        return ename;
+    }
+}
