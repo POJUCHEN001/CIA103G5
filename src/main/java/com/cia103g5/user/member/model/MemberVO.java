f5fb244656422b5844257b2b96bbf7bcb63572c4
P.J.Chen <lulu.chen231@gmail.com>
2024-11-24 16:40:10 +0800

    新增templates資料夾 及 css, js ..等

--- src/main/java/com/cia103g5/user/member/model/MemberVO.java
+++ src/main/java/com/cia103g5/user/member/model/MemberVO.java
@@ -1,6 +1,7 @@
 package com.cia103g5.user.member.model;
 
 import java.io.Serializable;
+import java.util.Arrays;
 import java.util.Date;
 
 import jakarta.persistence.Column;
@@ -60,6 +61,9 @@ public class MemberVO implements Serializable {
 
     @Column(name = "email_state")
     private Integer emailState;
+    
+    @Column(name ="key")
+    private String key;
 
     @Column(name = "registered_time")
     private Date registeredTime;
@@ -73,7 +77,7 @@ public class MemberVO implements Serializable {
     @Column(name = "gender")
 	@Min(value = 0, message = "性別只能為 0（不公開）、1（男）或 2（女）")
 	@Max(value = 2, message = "性別只能為 0（不公開）、1（男）或 2（女）")
-    private String gender;
+    private Integer gender;
     
 	@Column(name = "nickname", length = 50)
     private String nickname;
@@ -86,6 +90,169 @@ public class MemberVO implements Serializable {
 	
 	@Column(name = "status")
     private Integer status;
+	
+	// 建構子
+	public MemberVO() {
+		super();
+	}
+
+	public MemberVO(Integer memId,
+			@NotEmpty(message = "姓名請勿空白") @Pattern(regexp = "^[(一-龥)(a-zA-Z0-9_)]{2,10}$", message = "姓名: 只能是中、英文字母、數字和_ , 且長度必需在2到10之間") String name,
+			String account,
+			@NotEmpty(message = "密碼請勿空白") @Pattern(regexp = "^(?=.*[A-Z])(?=.*[a-z])(?=.*\\d)[A-Za-z\\d@$!%*?&]{6,20}$", message = "密碼必須包含大小寫字母及數字，且長度需在6到20字元之間") String password,
+			@NotEmpty(message = "E-mail請勿空白") @Pattern(regexp = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$", message = "請輸入有效的 Email 格式") String email,
+			String key, Integer emailState, Date registeredTime, byte[] photo, String phonenumber,
+			@Min(value = 0, message = "性別只能為 0（不公開）、1（男）或 2（女）") @Max(value = 2, message = "性別只能為 0（不公開）、1（男）或 2（女）") Integer gender,
+			String nickname, Integer points, String bankAccount, Integer status) {
+		super();
+		this.memId = memId;
+		this.name = name;
+		this.account = account;
+		this.password = password;
+		this.email = email;
+		this.key = key;
+		this.emailState = emailState;
+		this.registeredTime = registeredTime;
+		this.photo = photo;
+		this.phonenumber = phonenumber;
+		this.gender = gender;
+		this.nickname = nickname;
+		this.points = points;
+		this.bankAccount = bankAccount;
+		this.status = status;
+	}
+	
+
+	// getter & setter
+	public Integer getMemId() {
+		return memId;
+	}
+
+	public void setMemId(Integer memId) {
+		this.memId = memId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getAccount() {
+		return account;
+	}
+
+	public void setAccount(String account) {
+		this.account = account;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getKey() {
+		return key;
+	}
+
+	public void setKey(String key) {
+		this.key = key;
+	}
+
+	public Integer getEmailState() {
+		return emailState;
+	}
+
+	public void setEmailState(Integer emailState) {
+		this.emailState = emailState;
+	}
+
+	public Date getRegisteredTime() {
+		return registeredTime;
+	}
+
+	public void setRegisteredTime(Date registeredTime) {
+		this.registeredTime = registeredTime;
+	}
+
+	public byte[] getPhoto() {
+		return photo;
+	}
+
+	public void setPhoto(byte[] photo) {
+		this.photo = photo;
+	}
+
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+
+	public Integer getGender() {
+		return gender;
+	}
+
+	public void setGender(Integer gender) {
+		this.gender = gender;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public Integer getPoints() {
+		return points;
+	}
+
+	public void setPoints(Integer points) {
+		this.points = points;
+	}
+
+	public String getBankAccount() {
+		return bankAccount;
+	}
+
+	public void setBankAccount(String bankAccount) {
+		this.bankAccount = bankAccount;
+	}
+
+	public Integer getStatus() {
+		return status;
+	}
+
+	public void setStatus(Integer status) {
+		this.status = status;
+	}
+
+	
+	@Override
+	public String toString() {
+		return "MemberVO [memId=" + memId + ", name=" + name + ", account=" + account + ", password=" + password
+				+ ", email=" + email + ", key=" + key + ", emailState=" + emailState + ", registeredTime="
+				+ registeredTime + ", photo=" + Arrays.toString(photo) + ", phonenumber=" + phonenumber + ", gender="
+				+ gender + ", nickname=" + nickname + ", points=" + points + ", bankAccount=" + bankAccount
+				+ ", status=" + status + "]";
+	}
 	
 	
 }
